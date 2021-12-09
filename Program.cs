4cac7ba2b73ad9b073c0b87f92b62264e35b13fb
thecsharper <si@thecsharper.com>
2021-12-09 21:37:53 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+Compute(new Pinnable().Pin = 1);
+
+unsafe void Compute(object instance)
+{
+    fixed (ulong* ptr = &Unsafe.As<Pinnable>(instance).Pin)
+    {
+        Console.WriteLine(*ptr);
+    }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+internal sealed class Pinnable
+{
+    public ulong Pin;
+}
